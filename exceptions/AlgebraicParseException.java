81c0e30b1e64220e2f6c8cffa8e9d8c4c0cb80dc
Yulun Wu <yulun2007@gmail.com>
2025-04-11 15:04:25 -0400

    Added AlgebraicParseException javadoc

--- exceptions/AlgebraicParseException.java
+++ exceptions/AlgebraicParseException.java
@@ -1,5 +1,12 @@
 package exceptions;
-
+/**
+ * Custom exception class thrown when errors are encountered when parsing moves in algebraic notation<br>
+ * <p>
+ * Parsing algebraic notation moves is integral to the Terminal/Lanterna game loop as the loop cannot<br>
+ * continue reliably if a move cannot be generated from the user's input. Both the program state and <br>
+ * chess game state become invalid and thus we must either handle this situation (by prompting the user again)<br>
+ * or halt execution.
+ */
 public class AlgebraicParseException extends Exception {
     public AlgebraicParseException(String message){
         super(message);
